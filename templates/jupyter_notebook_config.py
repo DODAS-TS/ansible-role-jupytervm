d16916b54974be48a758b1cbd9e9f20653a17f73
dciangot <diego.ciangottini@gmail.com>
2020-04-08 18:26:03 +0200

    try hibryd content

--- templates/jupyter_notebook_config.py
+++ templates/jupyter_notebook_config.py
@@ -1,13 +1,32 @@
 from s3contents import S3ContentsManager
+from hybridcontents import HybridContentsManager
+from IPython.html.services.contents.filemanager import FileContentsManager
 
 c = get_config()
 
 c.NotebookApp.token = "{{ jupyter_token }}"
 
-# Tell Jupyter to use S3ContentsManager for all storage.
-c.NotebookApp.contents_manager_class = S3ContentsManager
-c.S3ContentsManager.access_key_id = "{{ s3_access_id}}"
-c.S3ContentsManager.secret_access_key = "{{ s3_access_key }}"
-c.S3ContentsManager.endpoint_url = "{{ s3_endpoint }}"
-c.S3ContentsManager.bucket = "{{ s3_bucket }}"
-c.S3ContentsManager.prefix = "{{ s3_mount_prefix }}"
+c.NotebookApp.contents_manager_class = HybridContentsManager
+
+c.HybridContentsManager.manager_classes = {
+    # Associate the root directory with an S3ContentsManager.
+    # This manager will receive all requests that don"t fall under any of the
+    # other managers.
+    "": S3ContentsManager,
+    # Associate /directory with a FileContentsManager.
+    "local_directory": FileContentsManager,
+}
+
+c.HybridContentsManager.manager_kwargs = {
+    # Args for root S3ContentsManager.
+    "": {
+        "access_key_id": "{{ s3_access_id}}",
+        "secret_access_key": "{{ s3_access_key }}",
+        "endpoint_url": "{{ s3_endpoint }}",
+        "bucket": "{{ s3_bucket }}",
+    },
+    # Args for the FileContentsManager mapped to /directory
+    "local_directory": {
+        "root_dir": "/opt/cloudadm",
+    },
+}
