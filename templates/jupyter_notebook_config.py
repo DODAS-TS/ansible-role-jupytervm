82629dc42469aabc06a4b04936b5c50a1cb91e07
dciangot <diego.ciangottini@gmail.com>
2020-04-09 09:00:31 +0200

    shared notebook folder s3

--- templates/jupyter_notebook_config.py
+++ templates/jupyter_notebook_config.py
@@ -12,21 +12,21 @@ c.HybridContentsManager.manager_classes = {
     # Associate the root directory with an S3ContentsManager.
     # This manager will receive all requests that don"t fall under any of the
     # other managers.
-    "": S3ContentsManager,
+    "shared_notebooks": S3ContentsManager,
     # Associate /directory with a FileContentsManager.
-    "local_directory": FileContentsManager,
+    "": FileContentsManager,
 }
 
 c.HybridContentsManager.manager_kwargs = {
     # Args for root S3ContentsManager.
-    "": {
+    "shared": {
         "access_key_id": "{{ s3_access_id}}",
         "secret_access_key": "{{ s3_access_key }}",
         "endpoint_url": "{{ s3_endpoint }}",
         "bucket": "{{ s3_bucket }}",
     },
     # Args for the FileContentsManager mapped to /directory
-    "local_directory": {
+    "": {
         "root_dir": "/opt/cloudadm",
     },
 }
